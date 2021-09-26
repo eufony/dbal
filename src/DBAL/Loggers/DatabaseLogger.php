825e3d318cc9aee94a91f0e1597b79a87094a8e4
Alpin H. Gencer <ah@gencer.us>
2021-09-26 11:49:18 +0200

    Move Loggers into DBAL

--- src/DBAL/Loggers/DatabaseLogger.php
+++ src/DBAL/Loggers/DatabaseLogger.php
@@ -17,7 +17,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-namespace Eufony\ORM\Loggers;
+namespace Eufony\DBAL\Loggers;
 
 use Eufony\DBAL\Database;
 use Eufony\DBAL\QueryException;
