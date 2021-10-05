f186078b929f716169adb416d01ba6f9f114bcde
Alpin Gencer <alpingencer@gmail.com>
2021-10-05 18:19:22 +0200

    Major directory restructure
    Signed-off-by: Alpin Gencer <alpingencer@gmail.com>

--- src/DBAL/Log/DatabaseLogger.php
+++ src/DBAL/Log/DatabaseLogger.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * The Eufony ORM Package
+ * The Eufony DBAL Package
  * Copyright (c) 2021 Alpin Gencer
  *
  * This program is free software: you can redistribute it and/or modify
@@ -20,7 +20,7 @@
 namespace Eufony\DBAL\Log;
 
 use Eufony\DBAL\Database;
-use Eufony\DBAL\QueryException;
+use Eufony\DBAL\Exception\QueryException;
 use Psr\Log\AbstractLogger;
 use Psr\Log\NullLogger;
 
--- src/DBAL/Log/LoggerTrait.php
+++ src/DBAL/Log/LoggerTrait.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * The Eufony ORM Package
+ * The Eufony DBAL Package
  * Copyright (c) 2021 Alpin Gencer
  *
  * This program is free software: you can redistribute it and/or modify
