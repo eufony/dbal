645de25dfa5b6c8636ae746378b75f9d262f1d28
Alpin Gencer <alpingencer@gmail.com>
2021-10-17 17:55:10 +0300

    Update PHPDoc for test cases
    Signed-off-by: Alpin Gencer <alpingencer@gmail.com>

--- tests/Unit/Cache/ArrayCacheTest.php
+++ tests/Unit/Cache/ArrayCacheTest.php
@@ -5,6 +5,9 @@ namespace Tests\Unit\Cache;
 use Eufony\ORM\Cache\ArrayCache;
 use Psr\SimpleCache\CacheInterface;
 
+/**
+ * Unit tests for `\Eufony\ORM\Cache\ArrayCache`.
+ */
 class ArrayCacheTest extends AbstractCacheTest {
 
     /** @inheritdoc */
