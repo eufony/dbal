31cdb43bf49a5b000f3883f4b9e76388568fa68f
Alpin H. Gencer <ah@gencer.us>
2021-10-19 13:40:59 +0300

    Add missing copyright headers for test sources

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,26 @@
 <?php
+/*
+ * Testsuite for the Eufony ORM Package
+ * Copyright (c) 2021 Alpin Gencer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 require __DIR__ . "/../vendor/autoload.php";
 
 echo "\n";
-echo "Testsuite for the Eufony ORM by Alpin Gencer.\n";
+echo "Testsuite for the Eufony ORM Package by Alpin Gencer.\n";
 echo "Using php.ini file: '" . php_ini_loaded_file() . "'\n";
 echo "\n";
 
